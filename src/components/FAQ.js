d663f4992f04e671fb4ae04b146499a614d3bcde
Catherine <21260778c@gmail.com>
2023-04-23 20:56:44 -0400

    updated colors in tailwind, updated colors of FAQ sections (waiting for new fonts)

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -14,20 +14,20 @@ const FAQ = ({content}) => {
       if (tokens[i]) {
         answers.push(
           <div className="faq-answer" key={tokens[i]}>
-            <ReactMarkdown style={{ color: "#FF34A0", "margin-bottom": "30px"}}>{tokens[i]}</ReactMarkdown>
+            <ReactMarkdown>{tokens[i]}</ReactMarkdown>
           </div>
         );
       }
     }
     let trigger_closed = <div className="faq-collapsible">
       <h2 className="faq-question" key={question}>{`${question}`}</h2>
-      <VscTriangleDown style={{ color: "#5561D3", minWidth: "16px" }} />
+      <VscTriangleDown style={{ color: "#FFFFFF", minWidth: "16px" }} />
 
     </div>
 
     let trigger_open = <div className="faq-collapsible">
       <h2 className="faq-question" key={question}>{`${question}`}</h2>
-      <VscTriangleUp style={{ color: "#5561D3", minWidth: "16px" }} />
+      <VscTriangleUp style={{ color: "#FFFFFF", minWidth: "16px" }} />
 
     </div>
 
@@ -42,7 +42,6 @@ const FAQ = ({content}) => {
     <div key="faqs" className="section">
         <a className="anchor" id="faqs"/>
         <h1 className="faq-title">FAQs</h1>
-        <ReactMarkdown className="faq-subtitle">We know you have a lot of questions, so check below for some of the most common answers. If you have other questions, drop us a line at [contact@pennapps.com](mailto:contact@pennapps.com).</ReactMarkdown>
         {output}
     </div>
   )
