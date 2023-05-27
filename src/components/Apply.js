93067ba352374fab1916d5840b5fc66f8f7991f2
Yuying Fan <yuyingfan@Yuyings-MacBook-Air.local>
2023-05-27 03:39:20 -0700

    #17 add ghosts

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import ghosts from '../images/ghosts.svg'
 
 const Apply = ({content, id}) => {
   return (
@@ -10,12 +11,25 @@ const Apply = ({content, id}) => {
             <h1>The Application Process</h1>
           </div>
           <div className="apply-text">
-            <p>Each year, PennApps receives thousands of qualified applications, but unfortunately, we can only accept a limited number of participants. To ensure fairness, we ask everyone to share a bit about themselves, so we can invite those who have shown interest and skill.</p>
-            <p>We don't expect everyone to have been to a hackathon before, in fact, we love first-time hackers having been there ourselves when we started! We just hope to bring those who are passionate about building something awesome and learning something new.</p>
-            <p>While we cannot accept everyone, we strive to build a diverse community of amazing hackers. Simply showcase your work, share why you want to attend PennApps, and we will do our best to bring you here! Penn students may register with their Penn emails when applying. Apply now at <a href="https://apply.pennapps.com/">apply.pennapps.com</a>!</p>
+            <p>Each year, PennApps receives thousands of qualified applications, but unfortunately,
+              we can only accept a limited number of participants. To ensure fairness, we ask
+              everyone to share a bit about themselves, so we can invite those who have shown
+              interest and skill.</p>
+            <p>We don't expect everyone to have been to a hackathon before, in fact, we love
+              first-time hackers having been there ourselves when we started! We just hope to bring
+              those who are passionate about building something awesome and learning something
+              new.</p>
+            <p>While we cannot accept everyone, we strive to build a diverse community of amazing
+              hackers. Simply showcase your work, share why you want to attend PennApps, and we will
+              do our best to bring you here! Penn students may register with their Penn emails when
+              applying. Apply now at <a href="https://apply.pennapps.com/">apply.pennapps.com</a>!
+            </p>
             <p></p>
           </div>
         </div>
+      </div>
+      <div id="ghosts">
+        <img src={ghosts}></img>
       </div>
     </div>
   )
