88fbb7c8b357b9d3fa52cfa83b775063e07be9b1
Anthony <geant@seas.upenn.edu>
2023-09-05 10:55:53 -0400

    reimport sponsors

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import FAQ from '../components/FAQ'
 import Heading from '../components/Heading'
 import Landing from '../components/Landing'
 import About from '../components/About'
-//import Sponsors from "../components/Sponsors"
+import Sponsors from "../components/Sponsors"
 import Logistics from '../components/Logistics'
 
 // markup
