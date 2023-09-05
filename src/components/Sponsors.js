c77710a47c8bebfb933cc19161d42226e785e765
Joy Liu <joyliu.q@gmail.com>
2023-09-05 16:22:29 -0400

    Add more logos and make sponsor logos make more sense

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -14,80 +14,200 @@ import balsamiq from '../images/sponsors_logo/Balsamiq.png';
 import axure from '../images/sponsors_logo/Axure.png';
 import wolfram from '../images/sponsors_logo/Wolfram-Language.png';
 import verbwire from '../images/sponsors_logo/Verbwire.svg';
+import metaphor from '../images/sponsors_logo/Metaphor.png';
+import mack from '../images/sponsors_logo/Mack-Institute.png';
+import nordvpn from '../images/sponsors_logo/NordVPN.png';
+import directai from '../images/sponsors_logo/DirectAI.png';
 
+function tierToRank(a) {
+  switch (a.tier) {
+    case "Title":
+      return 0;
+    case "Giga":
+      return 1;
+    case "Mega":
+      return 2;
+    case "Other":
+      return 3;
+    case "Prize":
+      return 4;
+    default:
+      return 5;
+  }
+}
 
+function tierToCol(a) {
+  const rank = tierToRank(a);
 
+  switch (rank) {
+    case 0:
+      return 12;
+    case 1:
+      return 6;
+    case 2:
+      return 4;
+    case 3:
+      return 3;
+    case 4:
+      return 2;
+    default:
+      return 1;
+  }
+}
+
+const SPONSORS = {
+  "Title": [
+    {
+      name: 'SIG',
+      link: 'https://www.sig.com',
+      logo: sig,
+    },
+  ],
+  "Giga": [
+    {
+      name: 'Bloomberg',
+      link: '',
+      logo: bloomberg,
+    },
+    {
+      name: 'Five Rings',
+      link: 'https://fiverings.com/',
+      logo: fiveRings,
+    },
+    {
+      name: 'Viam',
+      link: 'https://www.viam.com/',
+      logo: viam,
+    },
+  ],
+  "Mega": [
+    {
+      name: 'Capital One',
+      link: '',
+      logo: capitalOne,
+    },
+    {
+      name: 'USB Club',
+      link: 'https://usb.club/about',
+      logo: usbClubs,
+    },
+    {
+      name: 'Twilio',
+      link: 'https://www.twilio.com/en-us',
+      logo: twilio,
+    },
+    {
+      name: 'Jane Street',
+      link: 'https://www.janestreet.com/',
+      logo: janeStreet,
+    },
+    {
+      name: 'Stevens Capital Management',
+      link: 'https://www.scm-lp.com/',
+      logo: scm,
+    },
+  ],
+  "Other": [
+    {
+      name: 'Mack Institute',
+      link: 'https://mackinstitute.wharton.upenn.edu/',
+      logo: mack,
+    },
+    {
+      name: "Metaphor",
+      link: 'https://metaphor.systems/',
+      logo: metaphor,
+    },
+  ],
+  "Prize": [
+    {
+      name: 'Balsamiq',
+      link: 'https://balsamiq.com/',
+      logo: balsamiq,
+    },
+    {
+      name: 'Axure',
+      link: 'https://www.axure.com/',
+      logo: axure,
+    },
+    {
+      name: 'Wolfram Language',
+      link: '',
+      logo: wolfram,
+    },
+    {
+      name: 'Verbwire',
+      link: 'https://www.verbwire.com/',
+      logo: verbwire,
+    },
+    {
+      name: 'Meta',
+      link: 'https://www.metastudio.org/',
+      logo: meta,
+    },
+    {
+      name: "Nord Security",
+      link: '',
+      logo: nordvpn,
+    },
+    {
+      name: "Direct AI",
+      link: 'https://www.directai.com/',
+      logo: directai,
+    },
+  ]
+}
 
 function Sponsors() {
   return (
     <div className="section">
-      <a className="anchor" id="sponsors"/>
-      <img id="sponsor-title" src={sponsorTitle}></img>
+      <a className="anchor" id="sponsors" />
+      <img id="sponsor-title" src={sponsorTitle} alt="sponsor-title"></img>
       <div className="sponsor-logo-container">
-        <div id="SIG">
-          <a href="https://www.sig.com">
-            <img src={sig} alt="Sig"/>
-          </a>
-        </div>
-        <div id="Bloomberg">
-          <a href="">
-            <img src={bloomberg} alt="Bloomberg"/>
-          </a>
-        </div>
-        <div id="Five-Rings">
-          <a href="https://fiverings.com/ ">
-            <img src={fiveRings} alt="Five Rings"/>
-          </a>
-        </div>
-        <div id="Viam">
-          <a href="https://www.viam.com/">
-            <img src={viam} alt="Viam"/>
-          </a>
-        </div>
-        <div id="Capital-One">
-          <a href="">
-            <img src={capitalOne} alt="Viam"/>
-          </a>
-        </div>
-        <div id="USB-Club">
-          <a href="https://usb.club/about">
-            <img src={usbClubs} alt="USB Club"/>
-          </a>
-        </div>
-        <div id="Twilio">
-          <a href=" https://www.twilio.com/en-us">
-            <img src={twilio} alt="Twilio"/>
-          </a>
-        </div>
-        <div id="Jane-Street">
-          <a href=" https://www.janestreet.com/">
-            <img src={janeStreet} alt="Jane Street"/>
-          </a>
-        </div>
-        <div id="SCM">
-          <a href=" https://www.scm-lp.com/">
-            <img src={scm} alt="Stevens Capital Management"/>
-          </a>
-        </div>
-        <div id="Balsamiq">
-          <a href=" https://balsamiq.com/">
-            <img src={balsamiq} alt="Balsamiq"/>
-          </a>
-        </div>
-        <div id="Axure">
-          <a href=" https://www.axure.com/">
-            <img src={axure} alt="Axure"/>
-          </a>
-        </div>
-        <div id="Walfram">
-          <a href="">
-            <img src={wolfram} alt="Walfram Language"/>
-          </a>
-        </div>
-        <div id="Verbwire">
-          <a href="https://www.verbwire.com/">
-            <img src={verbwire} alt="Verbwire"/>
-          </a>
+        <div className="sponsor-logo-container-inner grid grid-cols-12 gap-4">
+          {SPONSORS["Title"].map((n) => (
+            <div class="col-span-12">
+              <a href={n.link}>
+                <img src={n.logo} alt={n.name} />
+              </a>
+            </div>
+          ))}
+          {
+            SPONSORS["Giga"].map((n) => (
+              <div className="col-span-6 row-auto">
+                <a href={n.link}>
+                  <img src={n.logo} alt={n.name} />
+                </a>
+              </div>
+            ))
+          }
+          {
+            SPONSORS["Mega"].map((n) => (
+              <div className="col-span-4 row-auto" >
+                <a href={n.link}>
+                  <img src={n.logo} alt={n.name} />
+                </a>
+              </div>
+            ))
+          }
+          {
+            SPONSORS["Other"].map((n) => (
+              <div className="col-span-3 row-auto" >
+                <a href={n.link}>
+                  <img src={n.logo} alt={n.name} />
+                </a>
+              </div>
+            ))
+          }
+          {
+            SPONSORS["Prize"].map((n) => (
+              <div className="col-span-2 row-auto" >
+                <a href={n.link}>
+                  <img src={n.logo} alt={n.name} />
+                </a>
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
