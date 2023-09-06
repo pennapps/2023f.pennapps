091332c586f6c0b3054c27586c111493b5fe18ed
Yuying Fan <yuyingfan@Yuyings-MacBook-Air.local>
2023-09-06 15:48:12 -0400

    added icons to logistics

--- src/components/Logistics.js
+++ src/components/Logistics.js
@@ -1,8 +1,8 @@
 import React from 'react'
+import calendarIcon from '../images/calendar-icon.svg'
+import trophyIcon from '../images/trophy-icon.svg'
 import background from '../images/LogisticsBackground.png' //change this file
 import mobileBackground from '../images/logistics_bg_mobile.png' //change this file
-import PrizesIcon from '../images/PrizesIcon.png'
-import ScheduleIcon from '../images/ScheduleIcon.png'
 import {MDXRenderer} from "gatsby-plugin-mdx";
 import {Link} from "gatsby";
 
@@ -17,10 +17,13 @@ function Logistics({content}) {
         <p> Interested in more details about the hackathon? Click the buttons below to learn
           more!</p>
         <div className="flex justify-center mx-auto">
-          <a href="/schedule/" className="mx-5">
-            Schedule</a>
-          <a href="/tracks-and-prizes/" className="mx-5">
-            Tracks and Prizes</a>
+          <IconSquare src={calendarIcon} name="Schedule" onClick={() => {
+            window.location.href = "/schedule/"
+          }}/>
+          <div className="w-16"></div>
+          <IconSquare src={trophyIcon} name="Tracks and Prizes" onClick={() => {
+            window.location.href = "/tracks-and-prizes/"
+          }}/>
         </div>
       </div>
     </div>
@@ -42,7 +45,7 @@ function Logistics({content}) {
 
 function IconSquare({name, src, onClick}) {
   return (
-    <div className="logistics-icon-square" onClick={onClick}>
+    <div className="logistics-icon-square cursor-pointer" onClick={onClick}>
       <div className="logistics-icon-square-container"><img src={src} className="max-h-[80%]"/>
       </div>
       <div>{name}</div>
